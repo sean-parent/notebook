7f01779ed5315dabf159389c9e54b8ec8515981d
Sean Parent <sean.parent@stlab.cc>
2018-08-22 10:44:32 -0700

    Checkpoint.

--- tools/start.sh
+++ tools/start.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -l|--lab)
+    LAB=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# end command processing
+
 export PATH="$HOME/miniconda3/bin:$PATH"
 
 trap "source activate notebook; jupyter notebook stop 8888" EXIT
 
 {
     source activate notebook
-    jupyter notebook
+    if [ $LAB = YES ]; then
+        jupyter lab
+    else
+        jupyter notebook
+    fi
 } & {
     source activate notebook;
     jupyter nbconvert ./better-code-class/*.ipynb --to=slides --reveal-prefix=../reveal.js \
