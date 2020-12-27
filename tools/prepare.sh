402e524a97f990bcd380c2223f7201169a1cfd0f
Sean <sean.parent@stlab.cc>
2020-12-27 14:09:47 -0800

    Commit to switch to laptop to continue work.

--- tools/prepare.sh
+++ tools/prepare.sh
@@ -22,15 +22,15 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-cp -Rf ./better-code-new/img ./docs/better-code-new/
-cp -Rf ./better-code-test/img ./docs/better-code-test/
-cp -Rf ./better-code-class/img ./docs/better-code-class/
+cp -ru ./better-code-new/img ./docs/better-code-new/
+cp -ru ./better-code-test/img ./docs/better-code-test/
+cp -ru ./better-code-class/img ./docs/better-code-class/
 
 #export PATH="/usr/local/Caskroom/miniconda/base/bin:$PATH"
 eval "$(conda shell.bash hook)"
 conda activate notebook
 
-if [ $SECTION == "all" ] || [ $SECTION == "class" ]; then 
+if [ $SECTION == "all" ] || [ $SECTION == "class" ]; then
 jupyter nbconvert ./better-code-class/*.ipynb --to=notebook --execute \
         --output-dir=./better-code-class/
 jupyter nbconvert ./better-code-class/*.ipynb --to=slides --reveal-prefix=../reveal.js \
@@ -38,14 +38,14 @@ jupyter nbconvert ./better-code-class/*.ipynb --to=slides --reveal-prefix=../rev
 fi
 
 
-if [ $SECTION == "all" ] || [ $SECTION == "test" ]; then 
+if [ $SECTION == "all" ] || [ $SECTION == "test" ]; then
 jupyter nbconvert ./better-code-test/*.ipynb --to=notebook --execute \
         --output-dir=./better-code-test/
 jupyter nbconvert ./better-code-test/*.ipynb --to=slides --reveal-prefix=../reveal.js \
     --output-dir=./docs/better-code-test --config=./slides-config/slides_config.py
 fi
 
-if [ $SECTION == "all" ] || [ $SECTION == "new" ]; then 
+if [ $SECTION == "all" ] || [ $SECTION == "new" ]; then
 jupyter nbconvert ./better-code-new/*.ipynb --to=notebook --execute \
         --output-dir=./better-code-new/
 jupyter nbconvert ./better-code-new/*.ipynb --to=slides --reveal-prefix=../reveal.js \
